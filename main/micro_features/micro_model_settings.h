14898f1ba033af7be6a01bd2a3f700057a289763
nguyen minh <nguyenminh180798@gmail.com>
2020-10-30 14:15:30 +0700

    update max audio sample size in setting for 30ms in 16000Hz

--- main/micro_features/micro_model_settings.h
+++ main/micro_features/micro_model_settings.h
@@ -22,7 +22,7 @@ limitations under the License.
 // The size of the input time series data we pass to the FFT to produce the
 // frequency information. This has to be a power of two, and since we're dealing
 // with 30ms of 16KHz inputs, which means 480 samples, this is the next value.
-constexpr int kMaxAudioSampleSize = 512;
+constexpr int kMaxAudioSampleSize = 480;
 constexpr int kAudioSampleFrequency = 16000;
 
 // The following values are derived from values used during model training.
