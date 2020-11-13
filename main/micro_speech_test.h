28c65c92df06ef3d8f630640ff25a915b36b30fc
nguyen minh <nguyenminh180798@gmail.com>
2020-11-13 10:13:16 +0700

    add performance of model test

---
+++ main/micro_speech_test.h
@@ -0,0 +1,15 @@
+#ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_MICRO_SPEECH_MICRO_SPEECH_TEST_
+#define TENSORFLOW_LITE_MICRO_EXAMPLES_MICRO_SPEECH_MICRO_SPEECH_TEST_
+
+// Expose a C friendly interface for main functions.
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+
+int main(int argc, char** argv);
+
+// #ifdef __cplusplus
+// }
+// #endif
+
+#endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_MICRO_SPEECH_MICRO_SPEECH_TEST_
